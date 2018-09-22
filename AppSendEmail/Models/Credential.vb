57596056547d723a17343d0ad6e815a8ef7a1aea
Fulvio <fulviocanducci@hotmail.com>
2018-09-21 21:30:41 -0300

    Credential

--- AppSendEmail/Models/Credential.vb
+++ AppSendEmail/Models/Credential.vb
@@ -1,7 +1,7 @@
 ﻿Public Class Credential
-    Public Shared Name As String = "Fúlvio"
-    Public Shared Email As String = "parapuasouza@gmail.com"
-    Public Shared Password As String = "123456@ab"
+    Public Shared Name As String = ""
+    Public Shared Email As String = ""
+    Public Shared Password As String = ""
     Public Shared SmtpAddress As String = "smtp.gmail.com"
     Public Shared EnableSsl As Boolean = True
     Public Shared Port As Integer = 587
